c28ab27c2ef0f64cff104e17b912562bc662fbe6
branden-eyk <brandeneyk@gmail.com>
2022-09-02 21:40:22 -0400

    Add menu and clearing buttons, and add functionality to clear button.

--- etchasketch.js
+++ etchasketch.js
@@ -1,4 +1,6 @@
 initialize();
+const clearBtn = document.querySelector('.rightBtn');
+clearBtn.addEventListener('click', clear);
 
 function initialize(screenSize = 16){
     const screenContainer = document.querySelector('.screen-container');
@@ -7,7 +9,7 @@ function initialize(screenSize = 16){
         const pixel = document.createElement('div');
         pixel.style.height = `${100 / screenSize}%`;
         pixel.style.width = `${100 / screenSize}%`;
-        pixel.style.backgroundColor = 'white'; 
+        pixel.style.backgroundColor = 'white';
         pixel.classList.add('pixel');
         pixel.addEventListener('mouseenter', handleHover);
         screenContainer.appendChild(pixel);
@@ -16,4 +18,20 @@ function initialize(screenSize = 16){
 
 function handleHover(e){
     e.currentTarget.style.backgroundColor = 'black';
+}
+
+function clear(e){
+    const body = document.querySelector('body');
+    const pixels = document.querySelectorAll('.pixel');
+    
+    body.classList.add('shake');
+    body.addEventListener('animationend', function(){
+        body.classList.remove('shake');
+        }, 
+        {once:true }
+    );
+
+    pixels.forEach((pixel) => {
+        pixel.style.backgroundColor ='white';
+    });
 }
